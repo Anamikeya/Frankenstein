27fee2a944b13f90398be48e31a3149740ca6b39
blom <blom.beats@gmail.com>
2021-03-29 02:26:51 +0200

    db context manager

--- database.py
+++ database.py
@@ -1,3 +1,30 @@
-def db_conn():
-    print("Connecting")
-    print("Connected")
+import sqlite3 as sql
+
+_db_file='database.db'
+
+
+class db(object):
+    """
+    Contextmanager for handling connection to database on disk
+
+
+    """
+    def __init__(self, file=_db_file, *args,**kwargs):
+        try:
+            print("Connecting db")
+            self.db = sql.connect(file)
+            print("Connected db")
+        except:
+            print("Couldnt connect to db")
+    def __enter__(self, *args,**kwargs):
+        print("Creating instance of db")
+        return self.db
+    def __exit__(self, type, value, traceback, *args,**kwargs):
+        print('Closing db')
+        self.db.close()
+        print('Closed db')
+
+with db():
+    print("innan")
+    print(db())
+    print("efter")
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import PySide2
 from database import *
 from pathlib import Path
 from glob import glob
+import numpy as np
 
 #Temporary folder location
 watchlist=[Path("C:\Assets\Cykelklubben\Leaves")]
@@ -10,9 +11,15 @@ watchlist=[Path("C:\Assets\Cykelklubben\Leaves")]
 folder=watchlist[0]
 
 for folder in watchlist:
-    files=folder.glob("*")
-    print(files)
+    files=list(folder.rglob("*"))
 
+    #file=files[0]
+    #print(file.name)
+
+    for x in files:
+        print(str(x))
+
+    print(f'{len(files)} files total')
 
 
 if __name__=="__main__":
