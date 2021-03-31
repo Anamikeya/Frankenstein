98396fec703ef87f3e1d8e75e2b96f795f9f0c0f
blom <blom.beats@gmail.com>
2021-03-31 21:26:49 +0200

    okje

--- main.py
+++ main.py
@@ -6,23 +6,28 @@ from glob import glob
 import numpy as np
 
 #Temporary folder location
-watchlist=[Path("C:\Assets\Cykelklubben\Leaves")]
+watchlist=[Path("C:\Assets\Cykelklubben\Leaves"),Path("C:\Assets\Cykelklubben\License")]
 
 folder=watchlist[0]
 
-for folder in watchlist:
-    files=list(folder.rglob("*"))
 
-    #file=files[0]
-    #print(file.name)
-
-    for x in files:
-        print(str(x))
-
-    print(f'{len(files)} files total')
 
 def scan(folder):
     print("Scanning folder")
+    allfiles = []
+
+    for folder in watchlist:
+        files = list(folder.rglob("*"))
+        allfiles.extend(files)
+        # file=files[0]
+        # print(file.name)
+
+        for x in files:
+            print(str(x))
+
+    print(f'{len(allfiles)} files total')
+
+    return allfiles
 
 
 if __name__=="__main__":
