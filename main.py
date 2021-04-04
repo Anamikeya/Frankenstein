5e092afb1a27c13b909fb66bf9a570074450a4fa
blom <blom.beats@gmail.com>
2021-04-04 04:24:40 +0200

    insert all data to db

--- main.py
+++ main.py
@@ -2,40 +2,42 @@ import PySide2
 from pathlib import Path
 from glob import glob
 import numpy as np
-from blom import init_logger,timer
+from blom import init_logger, timer
 import sqlite_utils as sql
 import sqlite3
 
-#Temporary folder location
-watchlist=[Path("C:\Assets\Cykelklubben\Leaves"),Path("C:\Assets\Cykelklubben\License")]
-folder=watchlist[0]
+# Temporary folder location
+watchlist = [Path("C:\Assets\Cykelklubben\Leaves"), Path("C:\Assets\Cykelklubben\License")]
+folder = watchlist[0]
 
-l=init_logger('frankenstein')
+l = init_logger('frankenstein')
 
-db=sql.Database('database.db')
-
-
-db["cats"].create({
-    "id": int,
-    "name": str,
-    "weight": float,
-}, pk="id")
-
-
+db = sql.Database('database.db')
 
 print(db.table_names())
 
-def scan(folder):
-    scan_timer=timer(format='s')
-    l.info("Scanning folder")
+#get all data
+#list(db[str(folder)].rows)
+
+def scan_all(watchlist):
+    scan_timer = timer(format='s')
+    l.info(f"Scanning all {len(watchlist)} folders in watchlist")
 
     allfiles = []
 
     for folder in watchlist:
-        files = list(folder.rglob("*"))
-        allfiles.extend(files)
-        # file=files[0]
-        # print(file.name)
+        l.info(f"Scanning folder {folder}")
+        files_folders = list(folder.rglob("*"))
+        l.info(f"Found {len(files_folders)} files and folders total")
+        files = [x for x in files_folders if x.is_file()]
+        l.info(f'Found {len(files)} files')
+        l.info(f'Found {len(files_folders)-len(files)} folders')
+
+
+        db[str(folder)].insert({"path": 'hgfhgf'})
+        for file in files:
+            db[str(folder)].insert({"path": str(file)})
+
 
         for x in files:
             l.info(str(x))
@@ -43,11 +45,10 @@ def scan(folder):
     l.info(f'{len(allfiles)} files total in {scan_timer}')
 
     return allfiles
-
 
 
 def scan_old(folder):
-    scan_timer=timer(format='s')
+    scan_timer = timer(format='s')
     l.info("Scanning folder")
 
     allfiles = []
@@ -66,7 +67,7 @@ def scan_old(folder):
     return allfiles
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     scan(folder)
 
     pass
