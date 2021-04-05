f0845a7698da45040475a1a2aa1c556f9be8f5ac
blombeats <blom.beats@gmail.com>
2021-04-05 12:02:19 +0200

    ui

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import PySide2
+from PySide6 import QtCore, QtWidgets, QtGui
 from pathlib import Path
-from glob import glob
 import numpy as np
 from blom import init_logger, timer
 import sqlite_utils as sql
-import sqlite3
+import os
+import sys
 
 l = init_logger('frankenstein')
 db = sql.Database('database.db')
@@ -58,96 +58,40 @@ def scan_folder_db(folder):
     l.info(f'Scan took {timer_scan_folder_db}')
     return files_list
 
-from PySide2 import QtWidgets
-from PySide2 import QtGui
-from PySide2 import QtCore
-import os
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
 
-from ui import main
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
 
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
 
-class MyFileBrowser(main.Ui_MainWindow, QtWidgets.QMainWindow):
-    def __init__(self, maya=False):
-        super(MyFileBrowser, self).__init__()
-        self.setupUi(self)
-        self.maya = maya
-        self.treeView.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-        self.treeView.customContextMenuRequested.connect(self.context_menu)
-        self.populate()
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
 
-    def populate(self):
-        #path = r"/Users/blom"
-        path = Path("/Users/blom")
-        self.model = QtWidgets.QFileSystemModel()
-        self.model.setRootPath((QtCore.QDir.rootPath()))
-        self.treeView.setModel(self.model)
-        self.treeView.setRootIndex(self.model.index(path))
-        self.treeView.setSortingEnabled(True)
-        self.button1=QtWidgets.QButton("hej")
+        self.button.clicked.connect(self.magic)
 
-    def context_menu(self):
-        menu = QtWidgets.QMenu()
-        open = menu.addAction("Open in new maya")
-        open.triggered.connect(self.open_file)
-
-        if self.maya:
-            open_file = menu.addAction("Open file")
-            open_file.triggered.connect(lambda: self.maya_file_operations(open_file=True))
-
-            import_to_maya = menu.addAction("Import to Maya")
-            import_to_maya.triggered.connect(self.maya_file_operations)
-
-            reference_to_maya = menu.addAction("Add reference to Maya")
-            reference_to_maya.triggered.connect(lambda: self.maya_file_operations(reference=True))
-
-        cursor = QtGui.QCursor()
-        menu.exec_(cursor.pos())
-
-    def open_file(self):
-        index = self.treeView.currentIndex()
-        file_path = self.model.filePath(index)
-        os.startfile(file_path)
-
-    def maya_file_operations(self, reference=False, open_file=False):
-        """
-        open ,  reference,  Import
-        :return:
-        """
-        index = self.treeView.currentIndex()
-        file_path = self.model.filePath(index)
-        import maya.cmds as cmds
-        if reference:
-            cmds.file(file_path, reference=True, type='mayaAscii', groupReference=True)
-        elif open_file:
-            file_location = cmds.file(query=True, location=True)
-            if file_location == 'unknown':
-                cmds.file(file_path, open=True, force=True)
-            else:
-                modify_file = cmds.file(query=True, modified=True)
-                if modify_file:
-                    result = cmds.confirmDialog(title='Opening new maya file',
-                                                message='This file is modified. do you want to save this file.?',
-                                                button=['yes', 'no'],
-                                                defaultButton='yes',
-                                                cancelButton='no',
-                                                dismissString='no')
-                    if result == 'yes':
-                        cmds.file(save=True, type='mayaAscii')
-                        cmds.file(file_path, open=True, force=True)
-                    else:
-                        cmds.file(file_path, open=True, force=True)
-                else:
-                    cmds.file(file_path, open=True, force=True)
-        else:
-            cmds.file(file_path, i=True, groupReference=True)
-
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    fb = MyFileBrowser()
-    fb.show()
-    app.exec_()
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+    sys.exit(app.exec_())
+
+
+
+
+
+
+
 
 
 
@@ -159,10 +103,7 @@ if __name__ == "__main__":
     
     """
 
-
-
-
-    #scan_all(watchlist)
+    # scan_all(watchlist)
     """
     folder=Path("Y:\Models")
 
