6b9d5e40584a9082aa2a3da401298b4ac0708aa2
blombeats <51360317+blombeats@users.noreply.github.com>
2021-04-14 00:59:09 +0200

    videotest working video player

---
+++ videotest.py
@@ -0,0 +1,112 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtMultimedia import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+
+import sys
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtCore import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QSlider
+
+from myVideoWidget import myVideoWidget
+from GUI import Ui_MainWindow
+
+class myMainWindow(Ui_MainWindow, QMainWindow):
+    def __init__(self):
+        super(Ui_MainWindow, self).__init__()
+        self.setupUi(self)
+        self.sld_video_pressed=False  #判断当前进度条识别否被鼠标点击
+        self.videoFullScreen = False   # 判断当前widget是否全屏
+        self.videoFullScreenWidget = myVideoWidget()   # 创建一个全屏的widget
+        self.player = QMediaPlayer()
+        self.player.setVideoOutput(self.wgt_video)  # 视频播放输出的widget，就是上面定义的
+        self.btn_open.clicked.connect(self.openVideoFile)   # 打开视频文件按钮
+        self.btn_play.clicked.connect(self.playVideo)       # play
+        self.btn_play.setText("play")
+        self.btn_stop.setText("stop")
+        self.btn_cast.setText("cast")
+
+        self.btn_stop.clicked.connect(self.pauseVideo)       # pause
+        self.btn_cast.clicked.connect(self.castVideo)        # 视频截图
+        self.player.positionChanged.connect(self.changeSlide)      # change Slide
+        self.videoFullScreenWidget.doubleClickedItem.connect(self.videoDoubleClicked)  #双击响应
+        self.wgt_video.doubleClickedItem.connect(self.videoDoubleClicked)   #双击响应
+        self.sld_video.setTracking(False)
+        self.sld_video.sliderReleased.connect(self.releaseSlider)
+        self.sld_video.sliderPressed.connect(self.pressSlider)
+        self.sld_video.sliderMoved.connect(self.moveSlider)   # 进度条拖拽跳转
+        self.sld_video.ClickedValue.connect(self.clickedSlider)  # 进度条点击跳转
+        self.sld_audio.valueChanged.connect(self.volumeChange)  # 控制声音播放
+        self.btn_cast.hide()
+
+    def castVideo(self):
+        screen = QGuiApplication.primaryScreen()
+        cast_jpg = './'+QDateTime.currentDateTime().toString("yyyy-MM-dd hh-mm-ss-zzz")+'.jpg'
+        screen.grabWindow(self.wgt_video.winId()).save(cast_jpg)
+
+    def volumeChange(self, position):
+        volume= round(position/self.sld_audio.maximum()*100)
+        print("vlume %f" %volume)
+        self.player.setVolume(volume)
+        self.lab_audio.setText("volume:"+str(volume)+"%")
+
+    def clickedSlider(self, position):
+        if self.player.duration() > 0:  # 开始播放后才允许进行跳转
+            video_position = int((position / 100) * self.player.duration())
+            self.player.setPosition(video_position)
+            self.lab_video.setText("%.2f%%" % position)
+        else:
+            self.sld_video.setValue(0)
+
+    def moveSlider(self, position):
+        self.sld_video_pressed = True
+        if self.player.duration() > 0:  # 开始播放后才允许进行跳转
+            video_position = int((position / 100) * self.player.duration())
+            self.player.setPosition(video_position)
+            self.lab_video.setText("%.2f%%" % position)
+
+    def pressSlider(self):
+        self.sld_video_pressed = True
+        print("pressed")
+
+    def releaseSlider(self):
+        self.sld_video_pressed = False
+
+    def changeSlide(self, position):
+        if not self.sld_video_pressed:  # 进度条被鼠标点击时不更新
+            self.vidoeLength = self.player.duration()+0.1
+            self.sld_video.setValue(round((position/self.vidoeLength)*100))
+            self.lab_video.setText("%.2f%%" % ((position/self.vidoeLength)*100))
+
+    def openVideoFile(self):
+        self.player.setMedia(QMediaContent(QFileDialog.getOpenFileUrl()[0]))  # 选取视频文件
+        self.player.play()  # 播放视频
+        print(self.player.availableMetaData())
+
+    def playVideo(self):
+        self.player.play()
+
+    def pauseVideo(self):
+        self.player.pause()
+
+    def videoDoubleClicked(self, text):
+
+        if self.player.duration() > 0:  # 开始播放后才允许进行全屏操作
+            if self.videoFullScreen:
+                self.player.setVideoOutput(self.wgt_video)
+                self.videoFullScreenWidget.hide()
+                self.videoFullScreen = False
+            else:
+                self.videoFullScreenWidget.show()
+                self.player.setVideoOutput(self.videoFullScreenWidget)
+                self.videoFullScreenWidget.setFullScreen(1)
+                self.videoFullScreen = True
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    vieo_gui = myMainWindow()
+    vieo_gui.show()
+    sys.exit(app.exec_())
